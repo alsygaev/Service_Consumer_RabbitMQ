24c6b7634c37c1cb602ce1c7f99df999f4da172d
alsygaev <alsygaev@yandex.ru>
2025-06-29 23:49:49 +0300

    add application.yaml

--- src/main/java/ru/education/spring/course/ServiceConsumerRabbitMQApplication.java
+++ src/main/java/ru/education/spring/course/ServiceConsumerRabbitMQApplication.java
@@ -1,17 +1,39 @@
 package ru.education.spring.course;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+@SpringBootApplication
+public class ServiceConsumerRabbitMQApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceConsumerRabbitMQApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceConsumerRabbitMQApplication.class, args);
+    }
+
+    @RabbitListener(queues = "operative_param")
+    public void receiveMessage(String message) {
+        logger.info("Получено сообщение: {}", message);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory("localhost");
+    }
+
+    @Bean
+    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        return factory;
     }
 }
